1299f632353d4b971673063b78cb569f37800e39
mahdi <mahdi_sahrai@yahoo.com>
2021-04-05 13:19:58 +0430

    the department of range (about Rxjava) was created

--- app/src/main/java/com/project/Repo/range/RangeActivity.java
+++ app/src/main/java/com/project/Repo/range/RangeActivity.java
@@ -2,11 +2,16 @@ package com.project.Repo.range;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.project.R;
 import com.project.databinding.ActivityRangeBinding;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class RangeActivity extends AppCompatActivity {
 
@@ -16,5 +21,32 @@ public class RangeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_range);
+
+        Observable.range(1,5)
+                .repeat(2)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        //------Disposable d==Subscribes-------------
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Integer integer) {
+                        Toast.makeText(RangeActivity.this, integer.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
     }
 }
